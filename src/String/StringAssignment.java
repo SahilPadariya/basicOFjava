ad3430b77b69d7fcc864eeb114a38388322326e8
SahilPadariya <sahilpa24@gmail.com>
2025-03-06 18:07:01 +0530

    string assignment

---
+++ src/String/StringAssignment.java
@@ -0,0 +1,118 @@
+package String;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class StringAssignment {
+    public static void main(String[] args) {
+        String fruit = "apple";
+        String name = "sallu";
+        String name2 = "apple";
+        String name1 = "Apple Bnana Mango Orange";
+        char[] arr = name.toCharArray();
+        int i = 0, j = arr.length - 1;
+        int[] countfreq = new int[128];
+
+        // 1. Print a string in reverse order
+//
+//        while (i < j) {
+//            char temp = arr[j];
+//            arr[j] = arr[i];
+//            arr[i] = temp;
+//            i++;
+//            j--;
+//        }
+//        name = new String(arr);
+//        System.out.println(name);
+//
+//       //3. check if a String is Palindrome
+//
+//        while (i < j) {
+//            if (arr[i] != arr[j]) {
+//                System.out.println("false");
+//                return;
+//            }
+//            i++;
+//            j--;
+//        }
+//        System.out.println("true");
+//
+//       //4. count number of words in a String (with / without split function)
+//
+//        int count = 1;
+//        for (i = 0; i < name1.length(); i++) {
+//            if (name1.charAt(i) == ' ') {
+//                count++;
+//            }
+//        }
+//        System.out.println(count);
+//
+//      //5. Program to check if two strings are same or not (without using inbuilt function .equals())
+//
+//        if (name == name2) {
+//            System.out.println("true");
+//            return;
+//        }
+//        if (name.length() == name2.length()) {
+//            for (i = 0; i < name.length(); i++) {
+//                if (name.charAt(i) != name2.charAt(i)) {
+//                    System.out.println("false");
+//                    return;
+//                }
+//            }
+//            System.out.println("true");
+//        } else {
+//            System.out.println("false");
+//        }
+//        6. Update a character in a string (without using .replace() function)
+
+//        for (int k = 0; k < arr.length; k++) {
+//            if (arr[k] == 'k') {
+//                arr[k] = 'l';
+//            }
+//            else {
+//                System.out.println("No avalible char");
+//            }
+//        }
+//        name = new String(arr);
+//        System.out.println(name);
+//
+////        7. Sort a string of lowercase
+//
+//        int index = 0;
+//        for (i = 0; i < arr.length; i++) {
+//            for (index = 0; index < arr.length - 1; index++) {
+//
+//                boolean isswapped=false;
+//
+//                if (arr[index] > arr[index + 1]) {
+//                    char temp = arr[index + 1];
+//                    arr[index + 1] = arr[index];
+//                    arr[index] = temp;
+//                }
+//                if (!isswapped){
+//                    break;
+//                }
+//            }
+//        }
+//        name = new String(arr);
+//        System.out.println(name);
+
+//        8.Print frequency of all the characters in string (expercted TC O(n), Aux SC O(1))
+
+        for (i = 0; i < fruit.length(); i++) {
+            char c = fruit.charAt(i);
+            countfreq[(int) c]++;
+        }
+        for (i=0;i< fruit.length();i++){
+            char c= fruit.charAt(i);
+            if (countfreq[i]>0){
+                System.out.println(c+"-->"+countfreq[(int)i]);
+            }
+        }
+            for (i = 0; i <fruit.length(); i++) {
+                char c = fruit.charAt(i);
+                System.out.println(c + "-> " + countfreq[(int)c]);
+            }
+        }
+    }
--- src/String/stringFunctions.java
+++ src/String/stringFunctions.java
@@ -1,26 +1,54 @@
 package String;
 
+import java.util.Arrays;
 import java.util.Locale;
 
 public class stringFunctions {
     public static void main(String[] args) {
-        String name="Sahil Padariya";
-        String name1="sahil";
-        System.out.println("lenght = "+name.length());
-        System.out.println("find index = "+name.charAt(0));
-        System.out.println("concat = "+name.concat(name1));
-        System.out.println("is empty = "+name.isEmpty());
-        System.out.println("equal ignore ther are ignore upper lower = "+name.equalsIgnoreCase(name1));
-        System.out.println("upper case = "+name.toUpperCase());
-        System.out.println("lower case = "+name.toLowerCase());
-        System.out.println("extra space delete = "+name1.trim());
-        System.out.println("cheak all same or not"+name.equals(name1));
-        System.out.println("Some start to end index iutput which you enter = "+name.substring(3,9));
-        System.out.println("give index for start substring = "+name.indexOf("Padariya"));
-        System.out.println("your speling misteck make right = "+name.replace("a","A"));
-        System.out.println("check in word substring have or no = "+name.contains("Sahil Padariya"));
-        System.out.println("check this word start yes or no = "+name.startsWith("Padariya"));
+        String name= new String("apple orange mango banana");
+        String name1="Apple";
+        String[]str={"apple","orange","mango"};
+//        System.out.println(name.isBlank());
+//        System.out.println(name1.replace('p','t'));
+//        System.out.println("lenght = "+name1.length());
+//        System.out.println("find index = "+name.charAt(5));
+//        System.out.println("concat = "+name.concat(name1));
+//        System.out.println("is empty = "+name.isEmpty());
+//        System.out.println("equal ignore ther are ignore upper lower = "+name.equalsIgnoreCase(name1));
+//        System.out.println("upper case = "+name.toUpperCase());
+//        System.out.println("lower case = "+name.toLowerCase());
+//        System.out.println("extra space delete = "+name1.trim());
+//        System.out.println("cheak all same or not"+name.equals(name1));
+//        System.out.println("Some start to end index iutput which you enter = "+name.substring(3,9));
+//        System.out.println("give index for start substring = "+name.indexOf("Padariya"));
+//        System.out.println("your speling misteck make right = "+name.replaceAll("Sahil","SAHIL"));
+//        System.out.println("check in word substring have or no = "+name.contains("S"));
+//        System.out.println("check this word start yes or no = "+name.startsWith("Padariya"));
+//        System.out.println(name1.replace('p','t'));
+//        for (int i=0;i<str.length;i++){
+//           str[i]=str[i].replaceAll("mango","banana");
+//        }
+//        System.out.println(Arrays.toString(str));
 
+//        System.out.println(name1.substring(0));
+//        System.out.println(name1.substring(0,2));
+//        System.out.println(name=Arrays.toString(name.split(" ",2)));
+//        System.out.println(Arrays.toString(name.split("n")));
+//        System.out.println(name1.compareTo("apple"));
+//        System.out.println(name1.compareToIgnoreCase("apple"));
+        int i=0,j=str.length-1;
+        while (i<j){
+            if (str[i].compareTo(str[j])>0){
+                String temp=str[i];
+                str[i]=str[j];
+                str[j]=temp;
+            }
+            else {
+                i++;
+                j--;
+            }
+        }
+        System.out.println(Arrays.toString(str));
 
     }
 
